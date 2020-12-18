82ad4d8f946a96e7bc68a410261374fe606039b4
Michael Sabin <msabin@gmail.com>
2020-12-18 11:14:11 -0500

    Day18

--- Avent2020/Program.cs
+++ Avent2020/Program.cs
@@ -6,7 +6,7 @@ namespace Avent2020
     {
         static void Main(string[] args)
         {
-            var prob = new Day17Pt2();
+            var prob = new Day18();
             prob.Run();
         }
     }
